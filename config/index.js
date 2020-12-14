fe35113d8d83098e23e491008c27a13d6c4e699e
Hzy0913 <hanzhaoy@chanjet.com>
2020-12-14 16:19:06 +0800

    feat: update development directory

---
+++ config/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  dev: {
+    host: 'localhost',
+    port: 8080,
+    proxyTable: {},
+    useEslint: true,
+    showEslintErrorsInOverlay: true,
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    assetsPublicPath: '/',
+    devtool: 'cheap-module-eval-source-map',
+    poll: false,
+    assetsSubDirectory: 'static',
+    cacheBusting: true,
+    cssSourceMap: true,
+    define: undefined
+  },
+  build: {
+    bundleAnalyzerReport: false,
+    index: path.resolve('./dist/index.html'),
+    assetsRoot: path.resolve('./dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: false,
+    devtool: 'eval',
+    define: undefined
+  }
+};
